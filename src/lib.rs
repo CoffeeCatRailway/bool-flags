5e6116d706a4555edc32e145000bde3205d3f589
CoffeeCatRailway <dragonlee1002@gmail.com>
2026-03-01 22:45:42 +1100

    Implement bitwise operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign};
 use minipaste::paste;
 
 macro_rules! create_flags {
@@ -57,6 +58,90 @@ macro_rules! create_flags {
 					write!(f, "0b{:0width$b}", self.0, width = size_of::<$vtype>() * 8)
 				}
 			}
+			
+			impl BitAnd for $name {
+				type Output = $name;
+				
+				fn bitand(self, rhs: Self) -> Self::Output {
+					Self(self.0 & rhs.0)
+				}
+			}
+			
+			impl BitAnd<$vtype> for $name {
+				type Output = $name;
+				
+				fn bitand(self, rhs: $vtype) -> Self::Output {
+					Self(self.0 & rhs)
+				}
+			}
+			
+			impl BitAndAssign for $name {
+				fn bitand_assign(&mut self, rhs: Self) {
+					self.0 &= rhs.0;
+				}
+			}
+			
+			impl BitAndAssign<$vtype> for $name {
+				fn bitand_assign(&mut self, rhs: $vtype) {
+					self.0 &= rhs;
+				}
+			}
+			
+			impl BitOr for $name {
+				type Output = $name;
+				
+				fn bitor(self, rhs: Self) -> Self::Output {
+					Self(self.0 | rhs.0)
+				}
+			}
+			
+			impl BitOr<$vtype> for $name {
+				type Output = $name;
+				
+				fn bitor(self, rhs: $vtype) -> Self::Output {
+					Self(self.0 | rhs)
+				}
+			}
+			
+			impl BitOrAssign for $name {
+				fn bitor_assign(&mut self, rhs: Self) {
+					self.0 |= rhs.0;
+				}
+			}
+			
+			impl BitOrAssign<$vtype> for $name {
+				fn bitor_assign(&mut self, rhs: $vtype) {
+					self.0 |= rhs;
+				}
+			}
+			
+			impl BitXor for $name {
+				type Output = $name;
+				
+				fn bitxor(self, rhs: Self) -> Self::Output {
+					Self(self.0 ^ rhs.0)
+				}
+			}
+			
+			impl BitXor<$vtype> for $name {
+				type Output = $name;
+				
+				fn bitxor(self, rhs: $vtype) -> Self::Output {
+					Self(self.0 ^ rhs)
+				}
+			}
+			
+			impl BitXorAssign for $name {
+				fn bitxor_assign(&mut self, rhs: Self) {
+					self.0 ^= rhs.0;
+				}
+			}
+			
+			impl BitXorAssign<$vtype> for $name {
+				fn bitxor_assign(&mut self, rhs: $vtype) {
+					self.0 ^= rhs;
+				}
+			}
 		}
 	};
 }
@@ -75,47 +160,125 @@ mod tests {
 
     #[test]
     fn functions() {
-		const FLAG_0: u8 = 0b00000001;
-		const FLAG_1: u8 = 0b00000010;
-		
-		assert_eq!(Flags8::from_u8(FLAG_0).0, FLAG_0);
-		assert_eq!(Flags8::from_u8(FLAG_1).0, FLAG_1);
-		assert_eq!(Flags8::from_u8(FLAG_0 | FLAG_1).0, FLAG_0 | FLAG_1);
-		
-		assert_eq!(Flags8::none().0, 0b00000000);
-		assert_eq!(Flags8::all().0, 0b11111111);
-		
-		let mut flags = Flags8::none();
-		assert_eq!(flags.get(0), false);
-		flags.flip(0);
-		assert_eq!(flags.get(0), true);
-		flags.clear(0);
-		assert_eq!(flags.get(0), false);
-		flags.set(0);
-		assert_eq!(flags.get(0), true);
+		macro_rules! check_functions {
+			($ftype:tt, $vtype:tt) => {
+				// Check from_TYPE
+				paste! {
+					assert_eq!($ftype::[<from_ $vtype>](1).0, 1);
+					assert_eq!($ftype::[<from_ $vtype>](2).0, 2);
+					assert_eq!($ftype::[<from_ $vtype>](1 | 2).0, 1 | 2);
+				}
+				
+				// Check none and all
+				assert_eq!($ftype::none().0, $vtype::MIN);
+				assert_eq!($ftype::all().0, $vtype::MAX);
+				
+				// Check get, flip, clear, set
+				let mut flags = $ftype::none();
+				assert_eq!(flags.get(0), false);
+				flags.flip(0);
+				assert_eq!(flags.get(0), true);
+				flags.clear(0);
+				assert_eq!(flags.get(0), false);
+				flags.set(0);
+				assert_eq!(flags.get(0), true);
+				
+				println!("{} functions passed", stringify!($ftype));
+			};
+		}
+		check_functions!(Flags8, u8);
+		check_functions!(Flags16, u16);
+		check_functions!(Flags32, u32);
+		check_functions!(Flags64, u64);
+		check_functions!(Flags128, u128);
+		#[cfg(feature = "usize")]
+		check_functions!(FlagsUSize, usize);
     }
 	
 	#[test]
-	fn display() {
+	fn display() { // Check Display is correct
 		assert_eq!(format!("{}", Flags8::none()), "0b00000000");
 		assert_eq!(format!("{}", Flags8::all()), "0b11111111");
+		println!("Flags8 display passed");
 		
 		assert_eq!(format!("{}", Flags16::none()), "0b0000000000000000");
 		assert_eq!(format!("{}", Flags16::all()), "0b1111111111111111");
+		println!("Flags16 display passed");
 		
 		assert_eq!(format!("{}", Flags32::none()), "0b00000000000000000000000000000000");
 		assert_eq!(format!("{}", Flags32::all()), "0b11111111111111111111111111111111");
+		println!("Flags32 display passed");
 		
 		assert_eq!(format!("{}", Flags64::none()), "0b0000000000000000000000000000000000000000000000000000000000000000");
 		assert_eq!(format!("{}", Flags64::all()), "0b1111111111111111111111111111111111111111111111111111111111111111");
+		println!("Flags64 display passed");
 		
 		assert_eq!(format!("{}", Flags128::none()), "0b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
 		assert_eq!(format!("{}", Flags128::all()), "0b11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+		println!("Flags128 display passed");
 		
 		#[cfg(feature = "usize")]
 		{
 			assert_eq!(format!("{}", FlagsUSize::none()), format!("0b{:0width$b}", usize::MIN, width = size_of::<usize>() * 8));
 			assert_eq!(format!("{}", FlagsUSize::all()), format!("0b{:0width$b}", usize::MAX, width = size_of::<usize>() * 8));
+			println!("FlagsUSize display passed");
 		}
+	}
+	
+	#[test]
+	fn bit_ops() {
+		macro_rules! check_bit_ops {
+			($ftype:tt, $vtype:tt) => {
+				let all = $ftype::all();
+				paste! {
+					let one = $ftype::[<from_ $vtype>](1);
+					let not_one = $ftype::[<from_ $vtype>](($vtype::MAX - 1));
+				}
+				assert_eq!(all & one, one);
+				assert_eq!(all & one.0, one);
+				
+				assert_eq!(all | one, all);
+				assert_eq!(all | one.0, all);
+				
+				assert_eq!(all ^ one, not_one);
+				assert_eq!(all ^ one.0, not_one);
+				
+				// And assign
+				let mut and_assign = $ftype::all();
+				and_assign &= one;
+				assert_eq!(and_assign, one);
+				
+				let mut and_assign = $ftype::all();
+				and_assign &= one.0;
+				assert_eq!(and_assign, one);
+				
+				// Or assign
+				let mut or_assign = $ftype::all();
+				or_assign |= one;
+				assert_eq!(or_assign, all);
+				
+				let mut or_assign = $ftype::all();
+				or_assign |= one.0;
+				assert_eq!(or_assign, all);
+				
+				// Xor assign
+				let mut xor_assign = $ftype::all();
+				xor_assign ^= one;
+				assert_eq!(xor_assign, not_one);
+				
+				let mut xor_assign = $ftype::all();
+				xor_assign ^= one.0;
+				assert_eq!(xor_assign, not_one);
+				
+				println!("{} bit ops passed", stringify!($ftype));
+			};
+		}
+		check_bit_ops!(Flags8, u8);
+		check_bit_ops!(Flags16, u16);
+		check_bit_ops!(Flags32, u32);
+		check_bit_ops!(Flags64, u64);
+		check_bit_ops!(Flags128, u128);
+		#[cfg(feature = "usize")]
+		check_bit_ops!(FlagsUSize, usize);
 	}
 }
